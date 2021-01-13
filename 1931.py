d2c877c53d6146a58cf1fbde1891bed257180416
KECO-00 <ksang98@icloud.com>
2021-01-13 23:43:54 +0900

    week2

--- 1931.py
+++ 1931.py
@@ -6,8 +6,7 @@ for i in range(num):
     start, finish = map(int, input().split())
     timetable.append([start, finish])
 
-s = sorted(timetable, key = lambda x : x[0])
-s = sorted(timetable, key = lambda x : x[1])
+s = sorted(timetable, key = lambda x : (x[1],x[0]))
 
 data = s[0][1]
 for i in range(1, num):
